4eb5b6006acef40c997f6222626a96c12576a741
Abdulrahman <abdulrahman@gamil.com>
2024-07-27 17:18:49 +0300

    p4
    add Task Card

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:untitled1/models/task.dart';
 import 'package:untitled1/views/home_cubit.dart';
 import 'package:custom_accordion/custom_accordion.dart';
+import 'package:untitled1/views/task_card.dart';
 import 'Home_Page_Drawer.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -17,11 +18,9 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: BlocBuilder<HomeCubit , HomeState>(
-        builder: (context , state) {
-          return Home_Page_Drawer();
-        }
-      ),
+      drawer: BlocBuilder<HomeCubit, HomeState>(builder: (context, state) {
+        return Home_Page_Drawer();
+      }),
       appBar: AppBar(
         backgroundColor: Colors.deepPurple,
         title: Text('Tasks'),
@@ -40,55 +39,9 @@ class _MyHomePageState extends State<MyHomePage> {
               : Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: state.PandOP!
-                      .map((Task t) => CustomAccordion(
-                    // why all the letters are capital
-                            title: t.title ,
-                            subTitle: t.priority.toString() ?? '---',
-                            backgroundColor: t.status == Status.failed
-                                ? Colors.red
-                                : t.status == Status.planned ||
-                                        t.status == Status.onProgress
-                                    ? Colors.blue[200]
-                                    : Colors.green,
-                            widgetItems: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text('${t.description}'),
-                                Text('${t.status}'),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    TextButton(
-                                      style: TextButton.styleFrom(
-                                          foregroundColor: Colors.blue),
-                                      onPressed: () => context
-                                          .read<HomeCubit>()
-                                          .setCompleted(t),
-                                      child: const Text('complete'),
-                                    ),
-                                    TextButton(
-                                      style: TextButton.styleFrom(
-                                          foregroundColor: Colors.grey),
-                                      onPressed: () => context
-                                          .read<HomeCubit>()
-                                          .setFailed(t),
-                                      child: const Text('Failed'),
-                                    ),
-                                    TextButton(
-                                      style: TextButton.styleFrom(
-                                          foregroundColor: Colors.pink),
-                                      onPressed: () => context
-                                          .read<HomeCubit>()
-                                          .RemoveTask(t),
-                                      child: const Text('Remove'),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ))
-                      .toList()),
+                      .map((Task t) => TaskCard(context, t))
+                      .toList(),
+                ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
